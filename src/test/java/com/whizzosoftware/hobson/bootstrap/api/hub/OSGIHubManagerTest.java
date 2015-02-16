9caf90048e92cbd574446430ea4996c64c959fab
Daniel Noguerol <dan@whizzosoftware.com>
2015-02-16 12:51:23 -0700

    Added "send test e-mail capability"

--- src/test/java/com/whizzosoftware/hobson/bootstrap/api/hub/OSGIHubManagerTest.java
+++ src/test/java/com/whizzosoftware/hobson/bootstrap/api/hub/OSGIHubManagerTest.java
@@ -1,54 +1,41 @@
-package com.whizzosoftware.hobson.bootstrap.api.action;
+/*******************************************************************************
+ * Copyright (c) 2015 Whizzo Software, LLC.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package com.whizzosoftware.hobson.bootstrap.api.hub;
 
 import com.whizzosoftware.hobson.api.HobsonRuntimeException;
-import com.whizzosoftware.hobson.api.action.meta.ActionMetaData;
 import com.whizzosoftware.hobson.api.hub.EmailConfiguration;
 import org.junit.Assert;
 import org.junit.Test;
 
 import javax.mail.Message;
-import java.util.HashMap;
-import java.util.Map;
 
-import static org.junit.Assert.assertEquals;
-
-public class EmailActionTest {
+public class OSGIHubManagerTest {
     @Test
     public void testCreateMessage() throws Exception {
         EmailConfiguration config = new EmailConfiguration("localhost", false, null, null, "foo@bar.com");
-        EmailAction a = new EmailAction("pluginId");
-
-        Map<String,Object> p = new HashMap<>();
+        OSGIHubManager a = new OSGIHubManager();
 
         try {
-            a.createMessage(null, config, p);
+            a.createMessage(null, config, null, null, null);
             Assert.fail("Should have failed with exception");
         } catch (HobsonRuntimeException ignored) {}
 
-        p.put(EmailConfiguration.PROP_MAIL_SENDER, "foo@bar.com");
-
         try {
-            a.createMessage(null, config, p);
+            a.createMessage(null, config, "bar@foo.com", null, null);
             Assert.fail("Should have failed with exception");
         } catch (HobsonRuntimeException ignored) {}
 
-        p.put(EmailAction.RECIPIENT_ADDRESS, "bar@foo.com");
-
         try {
-            a.createMessage(null, config, p);
+            a.createMessage(null, config, "bar@foo.com", "Subject", null);
             Assert.fail("Should have failed with exception");
         } catch (HobsonRuntimeException ignored) {}
 
-        p.put(EmailAction.SUBJECT, "Subject");
-
-        try {
-            a.createMessage(null, config, p);
-            Assert.fail("Should have failed with exception");
-        } catch (HobsonRuntimeException ignored) {}
-
-        p.put(EmailAction.MESSAGE, "Message");
-
-        Message m = a.createMessage(null, config, p);
+        Message m = a.createMessage(null, config, "bar@foo.com", "Subject", "Message");
         Assert.assertEquals(1, m.getFrom().length);
         Assert.assertEquals("foo@bar.com", m.getFrom()[0].toString());
         Assert.assertEquals(1, m.getAllRecipients().length);
